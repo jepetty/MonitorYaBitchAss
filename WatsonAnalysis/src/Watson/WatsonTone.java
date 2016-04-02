4ba9c0c44dc8ad8565ca0a6e27d9b3da43635e53
Jessica Petty <jessica.petty@colorado.edu>
2016-04-02 01:50:48 -0600

    We are connected to Watson yayayayayagit add --all

--- WatsonAnalysis/src/Watson/WatsonTone.java
+++ WatsonAnalysis/src/Watson/WatsonTone.java
@@ -1,16 +1,19 @@
 package Watson;
 
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 
 /** Class to analyze tone of messages using IBM Watson **/ 
 public class WatsonTone {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Jess is the best!");
 		
 		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
-		System.out.println(credentials.watsonUser);
+		service.setUsernameAndPassword(credentials.watsonUser,credentials.watsonPassword);
+		
+		String text = "Hi my name is Justin, muthafuckers. I have blond hair and blue eyes. Heil Hitler. I love Nazis.";
+		ToneAnalysis tone = service.getTone(text);
+		System.out.println(tone);
 	}
 
 }
