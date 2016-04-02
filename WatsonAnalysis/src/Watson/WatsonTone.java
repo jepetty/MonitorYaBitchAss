0c560d1310c8f3190ebdf7b14d24314873b2e150
Phantom1771 <justin.schiller@colorado.edu>
2016-04-02 16:04:51 -0600

    Set input for analysis to be the first command line argument

--- WatsonAnalysis/src/Watson/WatsonTone.java
+++ WatsonAnalysis/src/Watson/WatsonTone.java
@@ -5,13 +5,17 @@ import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 
 /** Class to analyze tone of messages using IBM Watson **/ 
 public class WatsonTone {
-	
+	public static String getText(String input) {
+		String Text = input;
+		return Text;
+	}
 	public static void main(String[] args) {
 		
 		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
 		service.setUsernameAndPassword(credentials.watsonUser,credentials.watsonPassword);
-		
-		String text = "Hi my name is Justin, muthafuckers. I have blond hair and blue eyes. Heil Hitler. I love Nazis.";
+		String text = getText(args[0]);
+		System.out.println(text);
+		//text = "Innapropriate content";
 		ToneAnalysis tone = service.getTone(text);
 		System.out.println(tone);
 	}
