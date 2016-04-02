f91927eb105da09a32a6a342021c7bd462a9bc0c
Jessica Petty <jessica.petty@colorado.edu>
2016-04-02 01:42:51 -0600

    movie movie for the groovy gitignore

--- WatsonAnalysis/src/Watson/WatsonTone.java
+++ WatsonAnalysis/src/Watson/WatsonTone.java
@@ -1,13 +1,16 @@
+package Watson;
+
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
 
 /** Class to analyze tone of messages using IBM Watson **/ 
 public class WatsonTone {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jess is the best!");
 		
 		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
+		System.out.println(credentials.watsonUser);
 	}
 
 }
