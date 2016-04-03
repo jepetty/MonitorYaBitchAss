e41f51468bad89df8c9447ce3296787cf4559494
Jessica Petty <jessica.petty@colorado.edu>
2016-04-02 20:54:38 -0600

    got parse file totally up and running, cleaned up the workspace ;)

--- parse.py
+++ parse.py
@@ -1,19 +1,30 @@
 import json
 import random
+from sys import argv
 from pprint import pprint
-from watson import analyze_tone
+from firebase import firebase
+from credentials import watsonUser, watsonPassword
+from watson_developer_cloud import ToneAnalyzerV3Beta
 import datetime
 import sys
 
-with open('data.json') as data_file:    
-    data = json.load(data_file)
+firebase = firebase.FirebaseApplication('https://sigjesus.firebaseio.com/', None)
+firebase.delete('/messages', None)
 
+# input_string is the string the user gives you to analyze
+script, input_string = argv
+messages=""
+
+tone_analyzer = ToneAnalyzerV3Beta(
+	username = watsonUser,
+	password = watsonPassword,
+	version = '2016-02-11')
+tone = tone_analyzer.tone(text=input_string)
 
 #These are the time ranges we can bug people about. 
 
 now = datetime.datetime.now()
 nowtest = now.replace(hour=4, minute=20, second=0, microsecond=0)
-print(nowtest)
 
 today8am = now.replace(hour=8, minute=0, second=0, microsecond=0)
 
@@ -37,26 +48,26 @@ sentenceOrdinalArray = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 
 
 #Arrays for the messages that could be presented when a certain emotion is present, chosen at random
 
-angerArray = ["you might come of a bit angry", "you might need to chill.", "you might need to take it down a notch.", "you sound a bit angry.", "you sound sort of moody."]
-disgustArray = ["try to be nicer", "A?", "you do not sound very enthused"]
-joyArray =["What a happy message", "happy2"]
-fearArray=["fear1", "fear2","fear3"]
-sadnessArray=["sad1","sad2","sad3","sad4"]
+angerArray = ["you might come off a bit angry...", "you may or may not sound like a dick", "you might need to chill.", \
+"you might need to take it down a notch.", "you sound a bit angry.", "you sound sort of moody."]
+disgustArray = ["you might want to try to be nicer", "you could rub someone the wrong way", "you do not sound very enthused"]
+joyArray =["you say some nice things", "you are very positive"]
+fearArray=["you sound a little uneasy", "you are being somewhat paranoid"]
+sadnessArray=["it sounds like something is wrong... Are you ok?","you sound a little bit under the weather", \
+"it sounds like you haz the sadz"]
 
 #These are the time ranges we can bug people about. 
 
 if (today11pm < nowtest< today1159pm):
-	print("Business hours are over, are you sure?")
+	print("Business hours are over, it might be a little late")
 elif (today12am < nowtest < today1259am):
 	print("Should you be texting this person this late")
 elif (today1am < nowtest< today230am):
-	print("Heading back from the bars? Just checking. Proceed?")
+	print("Heading back from the bars? Just checking.")
 elif (today231am < nowtest< today5am):
-	print("This is not an hour to text someone, you can probably wait a couple of hours, proceed?")
+	print("This is not an hour to text someone, you can probably wait a couple of hours")
 elif (today501am < nowtest< today630am):
-	print("It'a pretty early this person is probably sleeping, are you sure?")
-else: 
-	print("else")
+	print("It'a pretty early, this person is probably sleeping, are you sure?")
 
 def strip(uglystring):
 	return str(uglystring).replace('[','').replace("'",'').replace("'",'').replace(']','')
@@ -111,17 +122,21 @@ if(i==0 or i>2):
 		text= "In your text"
 		
 		if ((sentiment2["tone_name"] == "Anger") and (sentiment2["score"] >= .20)):
-			print(text +" " + angerChoice)
+			angerChoice = strip(random.sample(angerArray, 1))
+			messages = messages + text + " " + angerChoice + " "
 		elif((sentiment2["tone_name"] == "Disgust") and (sentiment2["score"] >= .20)):	
-			print(text +" " + disgustChoice)
+			disgustChoice = strip(random.sample(disgustArray, 1))
+			messages = messages + text + " " + disgustChoice + " "
 		elif((sentiment2["tone_name"] == "Fear") and (sentiment2["score"] >= .40)):	
-			print(text + " " +fearhoice)
+			fearChoice = strip(random.sample(fearArray, 1))
+			messages = messages + text + " " + fearChoice + " "
 		elif((sentiment2["tone_name"] == "Joy") and (sentiment2["score"] >= .40)):	
-			print(text + " " +joyChoice)
+			joyChoice = strip(random.sample(joyArray, 1))
+			messages = message + text + " " + joyChoice + " "
 		elif((sentiment2["tone_name"] == "Sadness") and (sentiment2["score"] >= .40)):	
-			print(text + " " +sadnessChoice)
-# print(i)
+			sadnessChoice = strip(random.sample(sadnessArray, 1))
+			messages = messages + text + " " + sadnessChoice + " "
 	
-
-
-	# Disgusted = ["You don't sound very enthused", "try to be nicer", "Are you breaking up with me?"]
+print(messages)
+messageJson = {'messages': messages}
+messages = firebase.post("/messages", messageJson)
