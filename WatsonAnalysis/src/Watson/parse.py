14987732ce225eb6884360b0c4966601b1d88c88
Sigrunn Sky <sigrunn.sky@colorado.edu>
2016-04-02 20:50:41 -0600

    More sentences

--- WatsonAnalysis/src/Watson/parse.py
+++ WatsonAnalysis/src/Watson/parse.py
@@ -45,11 +45,11 @@ sentenceOrdinalArray = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 
 
 #Arrays for the messages that could be presented when a certain emotion is present, chosen at random
 
-angerArray = ["you might come off a bit angry...", "you may or may not sound like a dick.", "you might need to chill.", "you might need to take it down a notch.", "you sound a bit angry.", "you sound sort of moody."]
-disgustArray = ["you might want to try to be nicer.", "you could rub someone the wrong way.", "you do not sound very enthused."]
-joyArray =["you say some nice things.", "you are very positive." , "you might be keeping your friend."]
-fearArray=["you sound a little uneasy.", "you are being somewhat paranoid.", "you do not sound confident", "you may be "]
-sadnessArray=["it sounds like something is wrong...are you ok?","you sound a little bit under the weather.","it sounds like you haz the sadz.","sad4"]
+angerArray = ["you might come off a bit angry...", "you may or may not sound like a dick.", "you might need to chill.", "you might need to take it down a notch.", "you do not sounds happy.", "you sound sort of moody.", "you sure you want that tone?"]
+disgustArray = ["you might want to try to be nicer.", "you could rub someone the wrong way.", "you do not sound very enthused.","you might want to reconsider"]
+joyArray =["you say some nice things.", "you are very positive." , "your friend will be happy.", "you sound cheery."]
+fearArray=["you sound a little uneasy.", "you are being somewhat paranoid.", "you do not sound confident"]
+sadnessArray=["it sounds like something is wrong...are you ok?","you sound a little bit under the weather.","it sounds like you haz the sadz."," you sound sad", "you sound unhappy", "you sound like you need a hug"]
 
 #These are the time ranges we can bug people about. 
 
